cad3461174076c6318e0a3ee135d4813b817201d
Sylvain Garcia <garcia.6l20@gmail.com>
2020-10-31 18:09:49 +0100

    add conan download

--- Conan.cmake
+++ Conan.cmake
@@ -1,3 +1,10 @@
+if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+ message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+ file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake"
+   "${CMAKE_BINARY_DIR}/conan.cmake")
+endif()
+include("${CMAKE_BINARY_DIR}/conan.cmake")
+
 macro(conan_requires)
 
   if(CONAN_EXPORTED) # in conan local cache
